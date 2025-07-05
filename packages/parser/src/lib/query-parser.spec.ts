90c29d0a6adce40b23d13c7062593f2a610923dd
Stefan Haas <stefan.haas.privat@gmail.com>
2025-07-06 00:13:42 +0200

    parser tests

---
+++ packages/parser/src/lib/query-parser.spec.ts
@@ -0,0 +1,59 @@
+import { QueryParser } from './query-parser.js';
+import { resolve } from 'node:path';
+
+describe('QueryParser', () => {
+  const queryParser: QueryParser = QueryParser.getInstance();
+
+  it('should parse sample-01.nxql', () => {
+    const filePath = resolve('../../queries/sample-01.nxql');
+    const query = queryParser.parseQueryFromFile(filePath);
+    expect(query).toEqual({
+      type: 'Query',
+      selection: {
+        type: 'All',
+      },
+      source: 'projects',
+      condition: {
+        type: 'ComparisonExpression',
+        left: 'name',
+        operator: '=',
+        right: '@nxdb/cli',
+      },
+    });
+  });
+
+  it('should parse sample-02.nxql', () => {
+    const filePath = resolve('../../queries/sample-02.nxql');
+    const query = queryParser.parseQueryFromFile(filePath);
+    expect(query).toEqual({
+      type: 'Query',
+      selection: {
+        type: 'All',
+      },
+      source: 'projects',
+      condition: {
+        type: 'InExpression',
+        value: 'type:library',
+        target: 'tags',
+      },
+    });
+  });
+
+  it('should parse sample-03.nxql', () => {
+    const filePath = resolve('../../queries/sample-03.nxql');
+    const query = queryParser.parseQueryFromFile(filePath);
+    expect(query).toEqual({
+      type: 'Query',
+      selection: {
+        type: 'All',
+      },
+      source: 'projects',
+      condition: {
+        type: 'ComparisonExpression',
+        left: 'hello',
+        operator: '=',
+        right: 'world',
+      },
+    });
+  });
+});
